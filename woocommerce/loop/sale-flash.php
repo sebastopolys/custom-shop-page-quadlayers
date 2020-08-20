4288362b9000b37eb0a866963a31e2b9be41315f
Seba Rossi <sebastopolys@users.noreply.github.com>
2020-08-20 13:35:35 -0300

    Add files via upload
    add loop files

--- woocommerce/loop/sale-flash.php
+++ woocommerce/loop/sale-flash.php
@@ -1,1 +1,28 @@
+<?php
+/**
+ * Product loop sale flash
+ *
+ * This template can be overridden by copying it to yourtheme/woocommerce/loop/sale-flash.php.
+ *
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see         https://docs.woocommerce.com/document/template-structure/
+ * @package     WooCommerce/Templates
+ * @version     1.6.4
+ */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
+
+global $post, $product;
+
+?>
+<?php if ( $product->is_on_sale() ) : 
+$an_gif = '<img src="https://i1.wp.com/tamingchaos.net/wp-content/uploads/2020/04/Sale-Gif.gif" width="100px" height="60px"/>';
+echo $an_gif;
+endif;
